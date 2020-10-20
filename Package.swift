8acb853de1f973dfc72f96b5345c2cef1b8256df
Serg Krivoblotsky <krivoblotsky@macpaw.com>
2020-10-20 10:21:38 +0300

    Update package

--- Package.swift
+++ Package.swift
@@ -2,18 +2,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "KYCAIDSDK",
+    name: "KYCAID",
     platforms: [
         .iOS(.v13)
     ],
     products: [
         .library(
-            name: "KYCAIDSDK",
-            targets: ["KYCAIDSDK"])
+            name: "KYCAID",
+            targets: ["KYCAID"])
     ],
     targets: [
         .binaryTarget(
-            name: "KYCAIDSDK",
+            name: "KYCAID",
             url: "https://nsbeep.com/KYCAIDSDK-release.xcframework.zip",
             checksum: "4cc7572150394365f04e3657a2d2d5bcc5842e3df0cc1834ccb9308dc2e99f36"
         )
