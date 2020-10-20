383718504701a4429fc9838dd79246268630874f
Serg Krivoblotsky <krivoblotsky@macpaw.com>
2020-10-20 10:18:13 +0300

    Update package

--- Package.swift
+++ Package.swift
@@ -6,10 +6,15 @@ let package = Package(
     platforms: [
         .iOS(.v13)
     ],
+    products: [
+        .library(
+            name: "KYCAIDSDK",
+            targets: ["KYCAIDSDK"])
+    ],
     targets: [
         .binaryTarget(
             name: "KYCAIDSDK",
-            url: "https://nsbeep.com/KYCAIDSDK.xcframework.zip",
+            url: "https://nsbeep.com/KYCAIDSDK-release.xcframework.zip",
             checksum: "4cc7572150394365f04e3657a2d2d5bcc5842e3df0cc1834ccb9308dc2e99f36"
         )
     ]
