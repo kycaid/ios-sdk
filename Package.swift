80d3d1dc39d60c961bb5284edfa41bb0a9bbe5ca
Serg Krivoblotsky <krivoblotsky@macpaw.com>
2021-01-07 18:08:48 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KYCAIDSDK",
-            url: "https://github.com/kycaid/ios-sdk/releases/tag/1.0.1/KYCAIDSDK.xcframework.zip",
+            url: "https://github.com/kycaid/ios-sdk/releases/download/1.0.1/KYCAIDSDK.xcframework.zip",
             checksum: "918aa58624a0137808e4e645ef7a9ca1c054b13d6e93f1339e3ae5ee2645fb5d"
         )
     ]
