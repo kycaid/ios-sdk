4516d4f1018cce2f2e39daf4b5704f4730ea2a2d
Vlad <vlad@xorum.io>
2025-04-23 15:11:42 +0200

    Add url and checksum to Package.swift

--- Package.swift
+++ Package.swift
@@ -11,11 +11,16 @@ let package = Package(
             name: "KYCAID",
             targets: ["KYCAIDSDK"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/roberthein/TinyConstraints", from: "4.0.2")
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.7.1")
+        .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.5.1")
+    ],
     targets: [
         .binaryTarget(
             name: "KYCAIDSDK",
             url: "https://github.com/kycaid/ios-sdk/releases/download/2.0.0/KYCAIDSDK.xcframework.zip",
-            checksum: "3f141522b7a257127a66958420461e428032fd52e0806ed764f22d095eda0e5d"
+            checksum: "aba01c0bfaa678b1ea2be59a5ce9563281b2fb298f299576bb0449664c80aee7"
         )
     ]
 )
