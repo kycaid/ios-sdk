f601475be7405fcccee00190387cef25673587ca
Vlad <vlad@xorum.io>
2025-09-02 13:29:14 +0200

    Add url and checksum to Package.swift

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KYCAIDSDK",
-            url: "https://github.com/kycaid/ios-sdk/releases/download/25.9.4/KYCAIDSDK.xcframework.zip",
-            checksum: "f0600b94d810fdacabf97401eb3d054b05f8ef7165e1f2d7734c8b52c0ed587d"
+            url: "https://github.com/kycaid/ios-sdk/releases/download/25.9.5/KYCAIDSDK.xcframework.zip",
+            checksum: "6a943143f7783eaf707a725112465b3c70019c4f11404611b16388020f668eec"
         ),
         .target(
             name: "KYCAID",
