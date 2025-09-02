b2ca18f62bf77958a35d0ed121c519a329fda475
Vlad <vlad@xorum.io>
2025-09-02 13:25:03 +0200

    Add url and checksum to Package.swift

--- Package.swift
+++ Package.swift
@@ -12,26 +12,26 @@ let package = Package(
             targets: ["KYCAID"])
     ],
     dependencies: [
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.21.0"),
-        .package(url: "https://github.com/SDWebImage/SDWebImageSVGCoder.git", from: "1.8.0"),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.7.0"),
-        .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.5.0")
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.7.1"),
+        .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.5.1"),
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.21.1"),
+        .package(url: "https://github.com/SDWebImage/SDWebImageSVGCoder.git", from: "1.8.0")
     ],
     targets: [
         .binaryTarget(
             name: "KYCAIDSDK",
-            url: "https://github.com/kycaid/ios-sdk/releases/download/25.9.3/KYCAIDSDK.xcframework.zip",
-            checksum: "9d1793702339e82ec755673ee713fbdebe18c649a2247dc80867414c2a2d721b"
+            url: "https://github.com/kycaid/ios-sdk/releases/download/25.9.4/KYCAIDSDK.xcframework.zip",
+            checksum: "f0600b94d810fdacabf97401eb3d054b05f8ef7165e1f2d7734c8b52c0ed587d"
         ),
         .target(
             name: "KYCAID",
             dependencies: [
                 "KYCAIDSDK",
-                .product(name: "SDWebImage", package: "SDWebImage"),
-                .product(name: "SDWebImageSVGCoder", package: "SDWebImageSVGCoder"),
                 .product(name: "RxSwift", package: "RxSwift"),
                 .product(name: "RxRelay", package: "RxSwift"),
-                .product(name: "Lottie", package: "lottie-spm")
+                .product(name: "Lottie", package: "lottie-spm"),
+                .product(name: "SDWebImage", package: "SDWebImage"),
+                .product(name: "SDWebImageSVGCoder", package: "SDWebImageSVGCoder")
             ],
             path: "Sources/KYCAID"
         )
